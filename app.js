5c163fe263df48f3e615f27b960cf8ce8282303a
DTIOC <oscar.cardenas@iems.edu.mx>
2026-08-18 17:47:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ class MusicTrainingApp {
         this.scaleType = 'Do Mayor';
         this.scaleNotes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4'];
         
-        this.webhookURL = 'https://script.google.com/macros/s/AKfycby4ZiaM4OhZerib7VpMMwuITvXi3_8qNSIbleVUisdiLTRvQP1Tt5vFt_XZkBvq2i3k/exec';
-        
+this.webhookURL = 'https://script.google.com/macros/s/AKfycby4ZiaM40hZerib7VpMMwuITvXi3_8qNSIbleVUisdiLTRvQP1Tt5vFt_XZkBvq2i3k/exec';        
         this.currentLevel = 1;
         this.successStreak = 0;
         this.isFamiliarizing = true;
